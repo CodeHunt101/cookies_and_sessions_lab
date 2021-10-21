78830a80823726ef32117af1e7de967184d65582
Harold Torres Marino <haroldtm55@gmail.com>
2021-10-21 20:26:35 +1100

    commit tree e62fdc20e712787350018258bc21a74e26b62d81

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,12 @@
+class ProductsController < ApplicationController
+  def index
+    # @product = Product.all
+    # byebug
+  end
+
+  def add
+    # @product = Product.find(params[:id])
+
+    # cart << @product.id
+  end
+end
